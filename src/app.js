13be8f78d60971e54e80035b845713e0d4ad9da6
Luis Fritzen <luisfafritzen@gmail.com>
2020-10-20 01:11:40 -0300

    feat: rota que deleta repositorios com ID requesitado

--- src/app.js
+++ src/app.js
@@ -13,7 +13,6 @@ const repositories = [];
 
 //should be able to list all repositories
 app.get("/repositories", (request, response) => {
-  // TODO
   return response.json(repositories);
 });
 
@@ -35,8 +34,18 @@ app.put("/repositories/:id", (request, response) => {
   // TODO
 });
 
+//should delete the repository with the requested id
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const findRepoIndex = repositories.findIndex(repository => 
+    repository.id === id
+  );
+  if(findRepoIndex >= 0){
+    repositories.splice(findRepoIndex, 1); 
+  }
+
+  return response.status(204).send([])
 });
 
 app.post("/repositories/:id/like", (request, response) => {
